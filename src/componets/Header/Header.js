0dc9eb6e755312ce7906a1749aa859e6a66df0fd
Stephen Rogers <stephenrogerszk@gmail.com>
2023-04-01 18:30:32 -0600

    Changes

--- src/componets/Header/Header.js
+++ src/componets/Header/Header.js
@@ -1,28 +1,27 @@
-import { useState } from 'react';
-import { 
-    Navbar, 
-    NavbarBrand,
-    Collapse,
-    NavbarToggler,
-    Nav, 
-    NavItem,
-    Container,
-    Col,
-    Row
-} from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import { GrHomeRounded } from "react-icons/gr";
-import './Header.css';
-import fs from '../../assets/images/fs.png';
-import sr from '../../assets/images/sr.png';
+import { useState } from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  Container,
+  Col,
+  Row,
+} from 'reactstrap'
+import { NavLink } from 'react-router-dom'
+import { GrHomeRounded } from 'react-icons/gr'
+import './Header.css'
+import fs from '../../assets/images/fs.png'
+import sr from '../../assets/images/sr.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faFacebook,
   faTwitter,
   faInstagram,
   faLinkedin,
-} from '@fortawesome/free-brands-svg-icons';
-
+} from '@fortawesome/free-brands-svg-icons'
 
 const Header = () => {
   const [isCollapsed, setIsCollapsed] = useState(true)
@@ -31,41 +30,62 @@ const Header = () => {
     setIsCollapsed(!isCollapsed)
   }
 
-    return (
+  return (
     <>
-    
-    <Container xs='6' sm='3' className='' >
-    
-    <button className='h4 rounded-3' style={{ alignItems: 'start'}} onClick={toggleCollapse}>{isCollapsed ? 'Open Tab' : 'Close Tab'}</button>
-    {isCollapsed ? (<div></div> ) : (
-    <div class="sidenav">
-        <img src={sr} width='90%'className='mt-5' alt='Stephen Rogers'/>
-        <img src={fs} width='70%' className='rounded-circle' id='headShot' alt='HeadShot'/>
-        <div>
-        <NavLink className='nav-link' to='/'>Home</NavLink>
-        <NavLink className='nav-link' to='/portfolio'>Portfolio</NavLink>
-        <NavLink className='nav-link' to='/services'>Services</NavLink>
-        <NavLink className='nav-link' to='/contact'>Contact</NavLink>
-        <NavLink className='nav-link' to='/about'>About</NavLink>
-        </div>
+      <Container xs="6" sm="3" className="">
+        <button
+          className="h4 rounded-3"
+          style={{ alignItems: 'start' }}
+          onClick={toggleCollapse}
+        >
+          {isCollapsed ? 'Open Tab' : 'Close Tab'}
+        </button>
+        {isCollapsed ? (
+          <div></div>
+        ) : (
+          <div class="sidenav">
+            <img src={sr} width="90%" className="mt-5" alt="Stephen Rogers" />
+            <img
+              src={fs}
+              width="70%"
+              className="rounded-circle"
+              id="headShot"
+              alt="HeadShot"
+            />
+            <div>
+              <NavLink className="nav-link" to="/">
+                Home
+              </NavLink>
+              <NavLink className="nav-link" to="/portfolio">
+                Portfolio
+              </NavLink>
+              <NavLink className="nav-link" to="/services">
+                Services
+              </NavLink>
+              <NavLink className="nav-link" to="/contact">
+                Contact
+              </NavLink>
+              <NavLink className="nav-link" to="/about">
+                About
+              </NavLink>
+            </div>
 
             <div className="d-flex justify-content-start ">
               <a href="https://www.facebook.com/">
                 <FontAwesomeIcon icon={faLinkedin} />
               </a>
               <a href="https://twitter.com/">
-                <FontAwesomeIcon icon={faTwitter}  />
+                <FontAwesomeIcon icon={faTwitter} />
               </a>
               <a href="https://www.instagram.com/">
-                <FontAwesomeIcon icon={faInstagram}  />
+                <FontAwesomeIcon icon={faInstagram} />
               </a>
             </div>
-        </div>
+          </div>
         )}
       </Container>
     </>
-  );
-};
+  )
+}
 
-
-export default Header;
+export default Header
