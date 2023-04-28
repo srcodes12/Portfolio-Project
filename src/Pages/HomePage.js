c792b03016dadd2b49ced0ae29f4c979328b6f01
Stephen Rogers <thelastcursader@gmail.com>
2023-04-27 23:43:03 -0600

    ,

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -12,7 +12,7 @@ import {
   ListGroupItem,
   ListGroup,
   Row,
-  CardHeader
+  CardHeader,
 } from 'reactstrap'
 import SubHeader from '../componets/Header/SubHeader'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
@@ -42,14 +42,22 @@ const HomePage = () => {
 
   return (
     <>
-    
       <div style={{ display: 'flex' }}>
         <Col>
-          <img src={hs}  className="d-flex  mt-5 "style={{ marginLeft: '20vw', width: '20vw' }} alt="Stephen Rogers" />
+          <img
+            src={hs}
+            className="d-flex  mt-5 "
+            style={{ marginLeft: '20vw', width: '20vw' }}
+            alt="Stephen Rogers"
+          />
         </Col>
         <Col>
           <div style={styles.collapsingContent}>
-            <Container fluid className="d-flex  mt-5 " style={{ justifyContent: 'center' }}>
+            <Container
+              fluid
+              className="d-flex  mt-5 "
+              style={{ justifyContent: 'center' }}
+            >
               <Card
                 className="d-flex boxstyle rounded-4"
                 style={{
@@ -92,51 +100,67 @@ const HomePage = () => {
           </div>
         </Col>
       </div>
-      <Container className='rounded-4' style={{ backgroundColor: '#f0f0f0', justifyContent: "end", marginRight: '10vw', marginLeft: '19vw', marginTop: '10vh', maxWidth: '70%',}} >
-      <Row className="justify-content-start align-items-center">
-        <Col xs={12} md={6}>
-          <h1>My Background</h1>
-        </Col>
-      </Row>
-      <hr></hr>
-        <Row className='row-content'>
-          <Col sm='6'>
-            <h1>bibendum </h1>
-              <h3>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien vel bibendum bibendum, velit sapien luctus velit, vel bibendum sapien sapien vel sapien. Sed euismod, sapien vel bibendum bibendum, velit sapien luctus velit, vel bibendum sapien sapien vel sapien. Sed euismod
-              </h3>
+      <Container
+        className="rounded-4"
+        style={{
+          backgroundColor: '#f0f0f0',
+          justifyContent: 'end',
+          marginRight: '10vw',
+          marginLeft: '19vw',
+          marginTop: '10vh',
+          maxWidth: '70%',
+        }}
+      >
+        <Row className="justify-content-start align-items-center">
+          <Col xs={12} md={6}>
+            <h1>My Background</h1>
           </Col>
-          <Col sm='6'>
+        </Row>
+        <hr></hr>
+        <Row className="row-content">
+          <Col sm="6">
+            <h1>bibendum </h1>
+            <h3>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              euismod, sapien vel bibendum bibendum, velit sapien luctus velit,
+              vel bibendum sapien sapien vel sapien. Sed euismod, sapien vel
+              bibendum bibendum, velit sapien luctus velit, vel bibendum sapien
+              sapien vel sapien. Sed euismod
+            </h3>
+          </Col>
+          <Col sm="6">
             <Card>
-              <CardHeader className='bg-primary text-white'>
-                  <h1>Lorem ipsum dolor sit amet</h1>
+              <CardHeader className="bg-primary text-white">
+                <h1>Lorem ipsum dolor sit amet</h1>
               </CardHeader>
               <CardBody>
-              <dl className='row'>
-                                <dt className='col-6'>Lorem</dt>
-                                <dd className='col-6'>ipsum</dd>
-                                <dt className='col-6'>dolor</dt>
-                                <dd className='col-6'>amet</dd>
-                                <dt className='col-6'>sit</dt>
-                                <dd className='col-6'>adipiscing</dd>
-                                <dt className='col-6'>euismod</dt>
-                                <dd className='col-6'>sapien</dd>
-                            </dl>
+                <dl className="row">
+                  <dt className="col-6">Lorem</dt>
+                  <dd className="col-6">ipsum</dd>
+                  <dt className="col-6">dolor</dt>
+                  <dd className="col-6">amet</dd>
+                  <dt className="col-6">sit</dt>
+                  <dd className="col-6">adipiscing</dd>
+                  <dt className="col-6">euismod</dt>
+                  <dd className="col-6">sapien</dd>
+                </dl>
               </CardBody>
             </Card>
           </Col>
           <Col>
             <Card>
               <CardBody>
-                <blockquote className='blockquote'>
+                <blockquote className="blockquote">
                   <p>
-                  sapien vel bibendum bibendum, velit sapien luctus velit, vel bibendum sapien sapien vel sapien. Sed euismod, sapien
+                    sapien vel bibendum bibendum, velit sapien luctus velit, vel
+                    bibendum sapien sapien vel sapien. Sed euismod, sapien
                   </p>
-                  <footer className='blockquote-footer'>
-                  bibendum bibendum,{' '}
-                      <cite title='Source Title'>
-                      sapien vel bibendum bibendum, velit sapien luctus velit, vel bibendum sapien sapien vel sapien. Sed euismod, sapien
-                      </cite>
+                  <footer className="blockquote-footer">
+                    bibendum bibendum,{' '}
+                    <cite title="Source Title">
+                      sapien vel bibendum bibendum, velit sapien luctus velit,
+                      vel bibendum sapien sapien vel sapien. Sed euismod, sapien
+                    </cite>
                   </footer>
                 </blockquote>
               </CardBody>
@@ -144,14 +168,11 @@ const HomePage = () => {
           </Col>
         </Row>
         <Row>
-          <Col xs='12'>
-            
-            <h1>
-            bibendum 
-            </h1>
+          <Col xs="12">
+            <h1>bibendum</h1>
           </Col>
         </Row>
-    </Container>
+      </Container>
     </>
   )
 }
