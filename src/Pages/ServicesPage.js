e9712cbf7b57bc2261d853694dc38a9f3445aa40
Stephen <stephenrogerszk@gmail.com>
2023-06-08 13:42:22 -0600

    Custom Border on services page

--- src/Pages/ServicesPage.js
+++ src/Pages/ServicesPage.js
@@ -1,94 +1,61 @@
-import { Container,
+import React from 'react';
+import {
+  Container,
   Card,
-  CardImg,
   CardBody,
-  CardSubtitle,
-  Button,
   CardTitle,
-  CardGroup,
   CardText,
 } from 'reactstrap';
 
-
 const ServicesPage = () => {
+  const containerStyle = {
+    height: 'auto',
+    background:
+      'linear-gradient(to right, yellow 4px, transparent 4px) 0 0,' +
+      'linear-gradient(to right, yellow 4px, transparent 4px) 0 100%,' +
+      'linear-gradient(to left, yellow 4px, transparent 4px) 100% 0,' +
+      'linear-gradient(to left, yellow 4px, transparent 4px) 100% 100%,' +
+      'linear-gradient(to bottom, yellow 4px, transparent 4px) 0 0,' +
+      'linear-gradient(to bottom, yellow 4px, transparent 4px) 100% 0,' +
+      'linear-gradient(to top, yellow 4px, transparent 4px) 0 100%,' +
+      'linear-gradient(to top, yellow 4px, transparent 4px) 100% 100%',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '20px 20px',
+    padding: '20px', 
+    display: 'inline-block',
+  };
+
   return (
-    <CardGroup>
-    <Card>
-      <CardImg
-        alt="Card image cap"
-        src="https://picsum.photos/318/180"
-        top
-        width="100%"
-      />
-      <CardBody>
-        <CardTitle tag="h5">
-          Card title
-        </CardTitle>
-        <CardSubtitle
-          className="mb-2 text-muted"
-          tag="h6"
-        >
-          Card subtitle
-        </CardSubtitle>
-        <CardText>
-          This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-        </CardText>
-        <Button>
-          Button
-        </Button>
-      </CardBody>
-    </Card>
-    <Card>
-      <CardImg
-        alt="Card image cap"
-        src="https://picsum.photos/318/180"
-        top
-        width="100%"
-      />
-      <CardBody>
-        <CardTitle tag="h5">
-          Card title
-        </CardTitle>
-        <CardSubtitle
-          className="mb-2 text-muted"
-          tag="h6"
-        >
-          Card subtitle
-        </CardSubtitle>
-        <CardText>
-          This card has supporting text below as a natural lead-in to additional content.
-        </CardText>
-        <Button>
-          Button
-        </Button>
-      </CardBody>
-    </Card>
-    <Card>
-      <CardImg
-        alt="Card image cap"
-        src="https://picsum.photos/318/180"
-        top
-        width="100%"
-      />
-      <CardBody>
-        <CardTitle tag="h5">
-          Card title
-        </CardTitle>
-        <CardSubtitle
-          className="mb-2 text-muted"
-          tag="h6"
-        >
-          Card subtitle
-        </CardSubtitle>
-        <CardText>
-          This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
-        </CardText>
-        <Button>
-          Button
-        </Button>
-      </CardBody>
-    </Card>
-  </CardGroup>
-    );
-  }
+    <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
+      <Container className='mt-5 d-flex justify-content-center align-items-center flex-column' style={containerStyle}>
+      <Card className='mb-3'>
+        <CardBody>
+          <CardTitle tag="h1">Front-end Development</CardTitle>
+          <CardText style={{ fontSize: '15px' }}>
+            Specializing in modern front-end technologies, I can help bring your vision to life with a responsive and interactive website.
+          </CardText>
+        </CardBody>
+      </Card>
+      <Card className='mb-3' style={{ width: '80%'}}>
+        <CardBody>
+          <CardTitle tag="h1">Custom UI Design</CardTitle>
+          <CardText style={{ fontSize: '15px',}}>
+            I provide custom UI design services tailored to your needs.
+            From conceptualizing to final implementation, I aim to create user-friendly designs that not only look good but also enhance user experience.
+          </CardText>
+        </CardBody>
+      </Card>
+      <Card className='mb-3'>
+        <CardBody>
+          <CardTitle tag="h1">Responsive Design</CardTitle>
+          <CardText style={{ fontSize: '15px' }}>
+            With a mobile-first approach, I ensure that your website looks great and functions flawlessly across all devices, be it desktop, tablet, or mobile.
+          </CardText>
+        </CardBody>
+      </Card>
+      </Container>
+    </div>
+  );
+};
+
 export default ServicesPage;
