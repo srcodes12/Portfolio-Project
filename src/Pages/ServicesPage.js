62d9eea4d160fe1e004154d4567aed5ba4e162f6
Stephen <stephenrogerszk@gmail.com>
2023-06-10 11:03:53 -0600

    Services page is collapsable with border

--- src/Pages/ServicesPage.js
+++ src/Pages/ServicesPage.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   Card,
   CardBody,
   CardTitle,
   CardText,
+  Collapse
 } from 'reactstrap';
+import './ServicesStyles.css';
 
 const ServicesPage = () => {
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [isOpen3, setIsOpen3] = useState(false);
+
+  const toggle1 = () => setIsOpen1(!isOpen1);
+  const toggle2 = () => setIsOpen2(!isOpen2);
+  const toggle3 = () => setIsOpen3(!isOpen3);
+
   const containerStyle = {
     height: 'auto',
     background:
@@ -24,31 +34,37 @@ const ServicesPage = () => {
   return (
     <div style={{display: 'flex', justifyContent: 'center', width: '100%', marginLeft: '5vw' }}>
       <Container className='mt-5 justify-content-center align-items-center flex-column' style={containerStyle}>
-      <Card className='mb-3'>
-        <CardBody>
-          <CardTitle tag="h1">Front-end Development</CardTitle>
-          <CardText style={{ fontSize: '15px' }}>
-            Specializing in modern front-end technologies, I can help bring your vision to life with a responsive and interactive website.
-          </CardText>
-        </CardBody>
-      </Card>
-      <Card className='mb-3' style={{ width: '100%'}}>
-        <CardBody>
-          <CardTitle tag="h1">Custom UI Design</CardTitle>
-          <CardText style={{ fontSize: '15px',}}>
-            I provide custom UI design services tailored to your needs.
-            From conceptualizing to final implementation, I aim to create user-friendly designs that not only look good but also enhance user experience.
-          </CardText>
-        </CardBody>
-      </Card>
-      <Card className='mb-3'>
-        <CardBody>
-          <CardTitle tag="h1">Responsive Design</CardTitle>
-          <CardText style={{ fontSize: '15px' }}>
-            With a mobile-first approach, I ensure that your website looks great and functions flawlessly across all devices, be it desktop, tablet, or mobile.
-          </CardText>
-        </CardBody>
-      </Card>
+        <Card className='mb-3'>
+          <CardTitle tag="h1" onClick={toggle1} className="collapsible-title">Front-end Development</CardTitle>
+          <Collapse isOpen={isOpen1}>
+            <CardBody>
+              <CardText style={{ fontSize: '15px' }}>
+                Specializing in modern front-end technologies, I can help bring your vision to life with a responsive and interactive website.
+              </CardText>
+            </CardBody>
+          </Collapse>
+        </Card>
+        <Card className='mb-3' style={{ width: '100%'}}>
+          <CardTitle tag="h1" onClick={toggle2} className="collapsible-title">Custom UI Design</CardTitle>
+          <Collapse isOpen={isOpen2}>
+            <CardBody>
+              <CardText style={{ fontSize: '15px',}}>
+                I provide custom UI design services tailored to your needs.
+                From conceptualizing to final implementation, I aim to create user-friendly designs that not only look good but also enhance user experience.
+              </CardText>
+            </CardBody>
+          </Collapse>
+        </Card>
+        <Card className='mb-3'>
+          <CardTitle tag="h1" onClick={toggle3} className="collapsible-title">Responsive Design</CardTitle>
+          <Collapse isOpen={isOpen3}>
+            <CardBody>
+              <CardText style={{ fontSize: '15px' }}>
+                With a mobile-first approach, I ensure that your website looks great and functions flawlessly across all devices, be it desktop, tablet, or mobile.
+              </CardText>
+            </CardBody>
+          </Collapse>
+        </Card>
       </Container>
     </div>
   );
